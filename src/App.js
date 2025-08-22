d4574d75745325ca6138829a1c12d8e2730bcdbb
rniemela77 <rvniemela@hotmail.com>
2025-08-21 22:27:42 -0700

    rewrite main and app as js, add custom component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import MyComponent from './components/MyComponent';
+
+function App() {
+    return (
+      <div>
+        <h1>React Concepts</h1>
+
+        <MyComponent />
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ src/components/MyComponent.js
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <div>
+      <p>This is a custom component</p>
+    </div>
+  );
+}
+
+export default MyComponent;
